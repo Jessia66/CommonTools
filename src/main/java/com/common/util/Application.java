942fcacf4087488d2bead959849a357a9a72cc61
jli2 <jli2@ecwise.com>
2018-03-02 10:26:43 +0800

    Create the initial common tools project

---
+++ src/main/java/com/common/util/Application.java
@@ -0,0 +1,13 @@
+package com.common.util;
+
+/**
+ * Hello world!
+ *
+ */
+public class Application 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
